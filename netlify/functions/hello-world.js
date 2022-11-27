5a1a86d933fc6ae810cfc236f3142ab72b994dde
livingfactory <111710162+livingfactory@users.noreply.github.com>
2022-11-27 14:12:47 -0500

    Implement chalk
    Add colorful text to terminal output

--- netlify/functions/hello-world.js
+++ netlify/functions/hello-world.js
@@ -1,4 +1,14 @@
+/* eslint-disable no-unused-vars */
+/* eslint-disable import/no-extraneous-dependencies */
+/* eslint-disable import/no-import-module-exports */
+import chalk from 'chalk';
+import fetch from 'node-fetch';
+
+const { DateTime } = require('luxon');
+
 exports.handler = async function () {
+  const date = DateTime.now();
+  console.log(chalk.blue(`${date}: Hello from my Netlify Functions!`));
   return {
     statusCode: 200,
     body: JSON.stringify({
--- netlify/functions/pokedex.js
+++ netlify/functions/pokedex.js
@@ -1,14 +1,26 @@
-// eslint-disable-next-line import/no-extraneous-dependencies, import/no-import-module-exports
+/* eslint-disable no-unused-vars */
+/* eslint-disable import/no-extraneous-dependencies */
+/* eslint-disable import/no-import-module-exports */
+import chalk from 'chalk';
 import fetch from 'node-fetch';
+
+const { DateTime } = require('luxon');
 
 // eslint-disable-next-line no-unused-vars, func-names
 exports.handler = async function (event, context) {
   const eventBody = JSON.parse(event.body);
+  const date = DateTime.now();
+  const color = eventBody.region === 'kanto' ? chalk.blue : chalk.green;
+
+  console.log(color(`${date}: Fetching data from PokeAPI!`));
+  console.log(color(`\teventBody.region: ${eventBody.region}`));
+
   // eslint-disable-next-line prefer-template
   const POKE_API = 'https://pokeapi.co/api/v2/pokedex/' + eventBody.region;
 
   const response = await fetch(POKE_API);
   const data = await response.json();
+  console.log(color(`\tNumber of entires: ${data.pokemon_entries.length}`));
 
   return {
     statusCode: 200,
