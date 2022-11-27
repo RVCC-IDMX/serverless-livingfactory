bbbdd50e948f3c94689d19ef1ab086806b4b5b77
livingfactory <111710162+livingfactory@users.noreply.github.com>
2022-11-27 14:05:07 -0500

    Update pokedex functions
    Pokedex functions now have two seperate buttons for different regions

--- netlify/functions/pokedex.js
+++ netlify/functions/pokedex.js
@@ -1,14 +1,19 @@
 // eslint-disable-next-line import/no-extraneous-dependencies, import/no-import-module-exports
 import fetch from 'node-fetch';
 
-exports.handler = async function () {
-  const POKE_API = 'https://pokeapi.co/api/v2/pokedex/kanto';
+// eslint-disable-next-line no-unused-vars, func-names
+exports.handler = async function (event, context) {
+  const eventBody = JSON.parse(event.body);
+  // eslint-disable-next-line prefer-template
+  const POKE_API = 'https://pokeapi.co/api/v2/pokedex/' + eventBody.region;
 
   const response = await fetch(POKE_API);
   const data = await response.json();
 
   return {
     statusCode: 200,
-    body: JSON.stringify(data),
+    body: JSON.stringify({
+      pokemon: data.pokemon_entries,
+    }),
   };
 };
